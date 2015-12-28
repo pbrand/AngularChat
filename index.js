61313abd8a67b0683033a66a40f1bf9576c9110c
PBrand <patrick.brand2@gmail.com>
2015-12-28 18:02:31 +0100

    Server supports names as well.

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var users = {};  
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('user \''+ socket.id +'\' connected');
+  socket.on('login', function(credentials) {
+      users[socket.id] = credentials.name;
+  });
+  console.log('user \''+ users[socket.id] +'\' connected');
   socket.on('disconnect', function(){
-    console.log('user \''+ socket.id + '\' disconnected');
+    console.log('user \''+ users[socket.id] + '\' disconnected');
   });
 socket.on('chat message', function(msg){
-    console.log('user \''+ socket.id +'\' says: \"'+msg+'\"');
+    console.log('user \''+ users[socket.id] +'\' says: \"'+msg+'\"');
     //io.emit('chat message', msg); // This emits to all.
-    socket.broadcast.emit('chat message', {author: socket.id, message: msg}); // Send message to everyone except the author
+    socket.broadcast.emit('chat message', {user: users[socket.id], message: msg}); // Send message to everyone except the author
   });
 });
 
