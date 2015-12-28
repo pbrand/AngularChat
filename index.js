c0273580f9af77cfe81b70d612ca4dea44bdaf77
PBrand <patrick.brand2@gmail.com>
2015-12-28 16:53:37 +0100

    Socket.io chat tuorial.

---
+++ index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('user \''+ socket.id +'\' connected');
+  socket.on('disconnect', function(){
+    console.log('user \''+ socket.id + '\' disconnected');
+  });
+socket.on('chat message', function(msg){
+    console.log('user \''+ socket.id +'\' says: \"'+msg+'\"');
+    //io.emit('chat message', msg); // This emits to all.
+    socket.broadcast.emit('chat message', msg); // Send message to everyone except the author
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
