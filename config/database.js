dc263f59436eb55abe71b8d3cd151be25107c772
Sander <sander.liebens@gmail.com>
2016-01-04 16:09:29 +0100

    Authentication complete

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+	'host' : 'localhost',
+	'user' : 'cs4105',
+	'password' : 'cs4105'
+    },
+	'database': 'AngularChat_insecure',
+    'users_table': 'users'
+};
