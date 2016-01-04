e5075f2d4ca30749fccded2c9ee4d12ad9293573
cs4105 <cs4105@localhost.localdomain>
2016-01-04 15:17:02 +0100

    Added gitignore and added user table to init_insequre.sql

--- init_insecure.sql
+++ init_insecure.sql
@@ -4,3 +4,5 @@ GRANT ALL ON AngularChat_insecure.* TO cs4105@localhost IDENTIFIED BY "cs4105";
 USE AngularChat_insecure;
 CREATE TABLE messages (id INT(8) NOT NULL AUTO_INCREMENT, message VARCHAR(50) NOT NULL, user VARCHAR(12) NOT NULL, PRIMARY KEY (id));
 INSERT INTO messages (message, user) VALUES ("This is a test!","testUser");
+CREATE TABLE users (id INT(8) NOT NULL AUTO_INCREMENT, username VARCHAR(12) NOT NULL, password VARCHAR(32) NOT NULL, PRIMARY KEY (id));
+INSERT INTO users (username, password) VALUES ("testUser", "testPassword");
